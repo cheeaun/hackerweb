39f80733ae277bfe54d3346992d82c3d196b09a4
Lim Chee Aun <cheeaun@gmail.com>
2011-10-16 20:22:48 +0800

    Use http://hndroidapi.appspot.com/ API plus YQL. Also, fixes orientationchange/resize event binding.

--- hn.js
+++ hn.js
@@ -3,19 +3,19 @@
 		$hnlist = d.getElementById('hnlist');
 	
 	w.loadNews = function(data){
-		if (!data || !data.items){
+		if (!data || !data.query || !data.query.results){
 			alert('Things borked, try reload plz?');
 			return;
 		}
 		var html = '',
 			a = d.createElement('a');
-		data.items.forEach(function(item){
+		data.query.results.items.forEach(function(item){
 			a.href = item.url;
 			var domain = a.hostname.match(/[^.]+\.[^.]+$/i)[0];
 			html += '<li>'
 					+ '<a href="' + item.url + '">'
 						+ '<b>' + item.title + ' <small>(' + domain + ')</small></b>'
-						+ '<span class="metadata">' + item.points + ' points by ' + item.postedBy + ' ' + item.postedAgo + '</span>'
+						+ '<span class="metadata">' + item.score + ' by ' + item.user + ' ' + item.time + '</span>'
 					+ '</a>'
 				+ '</li>';
 		});
@@ -32,7 +32,7 @@
 			body.style.height = w.innerHeight + 'px';
 		}, 1);
 	};
-	w.onorientationchange = scrollTop;
+	w[(typeof w.orientation != 'undefined') ? 'onorientationchange' : 'resize'] = scrollTop;
 	scrollTop();
 	
 	var $viewSections = d.querySelectorAll('.view>section');
@@ -44,8 +44,10 @@
 	if (news){
 		w.loadNews(news);
 	} else {
-		var script = d.createElement('script');
-		script.src = 'http://api.ihackernews.com/page?format=jsonp&callback=loadNews';
+		var script = d.createElement('script'),
+			q = 'select * from json where url="http://hndroidapi.appspot.com/news" and itemPath = "json.items"',
+			src = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent(q) + '&format=json&callback=loadNews&_maxage=600';
+		script.src = src;
 		body.appendChild(script);
 	}
 }(window, document);
