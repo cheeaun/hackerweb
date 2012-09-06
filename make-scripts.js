a23ff4ed60365a13326624466501e91063101fe5
Lim Chee Aun <cheeaun@gmail.com>
2012-09-06 09:17:59 +0800

    Split up the scripts into separate lines, for easier error debugging.

--- make-scripts.js
+++ make-scripts.js
@@ -5,20 +5,25 @@ var uglifyjs = require('uglify-js');
 var parser = uglifyjs.parser;
 var uglify = uglifyjs.uglify;
 
-fs.readdir('js', function(e, files){
-	if (e) throw e;
-	var code = '';
-	files.forEach(function(file){
-		if (/\.js$/i.test(file)){
-			code += fs.readFileSync('js/' + file, 'ascii');
-		}
-	});
-	
+var minify = function(code){
 	var ast = parser.parse(code);
 	ast = uglify.ast_mangle(ast);
 	ast = uglify.ast_squeeze(ast);
-	var finalCode = uglify.gen_code(ast);
-	fs.writeFile('scripts.js', finalCode, function(){
+	return uglify.gen_code(ast);
+}
+
+fs.readdir('js', function(e, files){
+	if (e) throw e;
+	var codes = '';
+	files.forEach(function(file){
+		if (/\.js$/i.test(file)){
+			var code = fs.readFileSync('js/' + file, 'ascii');
+			codes += '// ' + file + '\n'
+				+ minify(code) + ';\n';
+		}
+	});
+	
+	fs.writeFile('scripts.js', codes.replace(/\n$/, ''), function(){
 		console.log('scripts.js created.');
 	});
 });
