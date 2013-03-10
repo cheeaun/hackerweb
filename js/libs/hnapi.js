87f4f5de7f00eae113d352fabd3629b1e7149869
Lim Chee Aun <cheeaun@gmail.com>
2013-03-10 14:40:07 +0800

    Remove modulus.io

--- js/libs/hnapi.js
+++ js/libs/hnapi.js
@@ -80,8 +80,7 @@
 			'http://node-hnapi.ap01.aws.af.cm/', // AWS (Asia Pacific)
 			'http://node-hnapi-hp.hp.af.cm/', // HP Cloud
 			'http://node-hnapi-rs.rs.af.cm/', // Rackspace Cloud
-			'http://node-hnapi.azurewebsites.net/', // Windows Azure
-			'https://nodehnapi-5330.onmodulus.net/' // Modulus.io
+			'http://node-hnapi.azurewebsites.net/' // Windows Azure
 		]
 	};
 
