e04c92f4497e7fcabca8230b9c899bba128d95f6
Lim Chee Aun <cheeaun@gmail.com>
2013-04-09 22:42:25 +0800

    Report the original and minified file sizes

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,9 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		uglify: {
+			options: {
+				report: 'min'
+			},
 			web: {
 				options: {
 					sourceMap: 'js/hw-web.min.js.map',
