fef983537b5266ac6562a86d2d9a514cd0b832f1
Lim Chee Aun <cheeaun@gmail.com>
2015-11-07 20:49:13 -0800

    Web version now uses system fonts! (not Open Sans)

--- assets/js/hw-web.js
+++ assets/js/hw-web.js
@@ -75,16 +75,3 @@
 
 	window.onload = hw.init;
 })(window);
-
-WebFontConfig = {
-	google: { families: [ 'Open+Sans:400,400italic,600,600italic' ] }
-};
-(function() {
-	var wf = document.createElement('script');
-	wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-		'://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-	wf.type = 'text/javascript';
-	wf.async = 'true';
-	var s = document.getElementsByTagName('script')[0];
-	s.parentNode.insertBefore(wf, s);
-})();
